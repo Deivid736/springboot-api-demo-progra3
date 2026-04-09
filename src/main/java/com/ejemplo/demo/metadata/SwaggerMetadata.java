05d8a7c267df83b6756ee95302267fc61ede49bb
Deivid Alberto Guerra Carpio <dguerrac7@miumg.edu.gt>
2026-04-09 00:52:40 -0600

    agrego paso 7

---
+++ src/main/java/com/ejemplo/demo/metadata/SwaggerMetadata.java
@@ -0,0 +1,25 @@
+package com.ejemplo.demo.metadata;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerMetadata {
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Workshop Spring Boot 3.x")
+                        .version("2.6.0")
+                        .description("Workshop de la UMG")
+                        .contact(new Contact()
+                                .name("Deivid Alberto Guerra Carpio")
+                                .email("dguerrac7@miumg.edu.gt")));
+
+
+
+}
+}
